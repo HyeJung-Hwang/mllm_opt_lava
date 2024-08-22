62e1e33c82d8d2f01a1c9bf479b3e844a35f4d98
Hyejung-Hwang <seathat33@gmail.com>
2024-08-22 15:22:34 +0900

    feat: 각 dataset 마다 하나의 class 구현

---
+++ dataset.py
@@ -0,0 +1,17 @@
+from torch.utils.data import Dataset
+
+
+class CocoDataset(Dataset):
+    pass
+
+class GqaDataset(Dataset):
+    pass
+
+class OcrVqaDataset(Dataset):
+    pass
+
+class TextVqaDataset(Dataset):
+    pass
+
+class VgDataset(Dataset):
+    pass
